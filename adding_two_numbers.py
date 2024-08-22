f8cec5025db90cc960761b9e62fef04427169c15
sowmyasunkara-knsass <23P31A4444@acet.ac.in>
2024-08-22 11:28:03 +0530

    Add files via upload

---
+++ adding_two_numbers.py
@@ -0,0 +1,14 @@
+ '''#This is first program -> Comment
+print("Hello World")
+a=10
+b=20
+print(a+b)
+print(10,end=' ')
+print('a', 'b' 'c', sep=',',end= 'sowmya')
+print('hello')
+print([10,20,30,])
+print("hi",end='____')
+print("python")'''
+a=int(input('Enter the value of a: '))
+b=int(input('Enter the value of b: '))
+print(a+b)
