92689f0509ba33349c6b92206e1caa8a23a12f84
sowmyasunkara-knsass <23P31A4444@acet.ac.in>
2024-08-30 09:42:43 +0530

    Create Courses.py

---
+++ Courses.py
@@ -0,0 +1,13 @@
+c1 = "reactjs"
+c2 = "mongodb"
+c3 = "nodejs"
+c4 = "blockchain"
+s1 = (c1,c2)
+s2 = (c3,c4)
+s3 = (c1,c3)
+s4 = (c3,c4)
+s5 = (c1,c2)
+s6 = (c3,c4)
+courses = {s1,s2,s3,s4,s5,s6}
+print(courses)
+print(len(courses))
