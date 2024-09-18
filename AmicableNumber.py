360ebea5ddbae2f8c364b80a78906e03614475fa
sowmyasunkara-knsass <23P31A4444@acet.ac.in>
2024-09-18 09:47:47 +0530

    Create AmicableNumber.py

---
+++ AmicableNumber.py
@@ -0,0 +1,14 @@
+a=int(input())
+b=int(input())
+s=0
+sum=0
+for i in range(1,a):
+	if a%i==0
+	s+=i
+for j in range(1,b):
+	if b%j==0
+	sum+=j
+if s==sum:
+print("Amicable number")
+else:
+print("Not Amicable number")
