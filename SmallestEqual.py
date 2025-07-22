66ce591527b9b1f5a0c960effdaaacc4208c7fdd
sowmyasunkara-knsass <23P31A4444@acet.ac.in>
2025-07-22 19:02:48 +0530

    Create SmallestEqual.py

---
+++ SmallestEqual.py
@@ -0,0 +1,6 @@
+class Solution:
+    def smallestEqual(self, nums: List[int]) -> int:
+        for i in range(0,len(nums)):
+            if i%10==nums[i]:
+                return i    
+        return -1
